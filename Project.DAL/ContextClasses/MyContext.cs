f9e1969e669baadf0a74d0f01104449d543cefc5
Boran Kaya <borankaya210@gmail.com>
2023-04-01 05:10:39 +0300

    Add project files.

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,33 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public  class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+           
+
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+
+    }
+}
---
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Project.ViewModels.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register() //Üye ol
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AppUserVM appUser) //Üye ol
+        {
+            return View();
+        }
+        public ActionResult Login()// Giriş yap
+        {
+
+            return View();
+        }
+    }
+}
